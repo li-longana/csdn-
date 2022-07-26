d3932e84039aca07e706e532e3036910d9ad8756
930614591@qq.com <930614591@qq.com>
2022-07-26 11:57:12 +0800

    CSDN++ 0.2 DEV 加入随机值，ua变化更多样

--- 0.2 DEV/csdn++.py
+++ 0.2 DEV/csdn++.py
@@ -1,5 +1,6 @@
 import main
 import time
+import random
 print("CSDN++ 0.2 Bata版本启动中。。",end="\r")
 time.sleep(1)
 print("CSDN++ 0.2 Bata版本启动中。。。。",end="\r")
@@ -12,7 +13,8 @@ print("配置文件加载成功。。。。。")
 time.sleep(1)
 print("预设推广次数为：",config['count'],"\n预设推广时间间隔为：",config['interval'],"\n预设推广地址为：",config['url'])
 time.sleep(1)
-
+luck=random.randint(1,100)
+print("你的幸运值为：",luck)
 ua=main.Openua('ua.txt').ua
 # 加载ua
 
@@ -23,7 +25,7 @@ while(z):
     # 加载文章
     cs=0
     for j in url:
-        uatemp=ua[cs%(len(ua))]
+        uatemp=ua[cs%(len(ua)+luck)]
         main.gogogo(j,uatemp)
         time.sleep(int(config['interval']))# 冷却时间
         cs=cs+1
