2d188e7421b3d0e21e6a47235f739088a878e367
930614591@qq.com <930614591@qq.com>
2022-07-26 16:08:23 +0800

    CSDN++ 0.3 Bata

---
+++ 0.3 Bata/csdn++.py
@@ -0,0 +1,40 @@
+import main
+import time
+import random
+print("CSDN++ 0.3 Bata版本启动中。。",end="\r")
+time.sleep(1)
+print("CSDN++ 0.3 Bata版本启动中。。。。",end="\r")
+time.sleep(1)
+print("CSDN++ 0.3 Bata版本启动中。。。。。。")
+time.sleep(1)
+config=main.Openconfig('config.txt').config
+# 加载配置文件
+print("配置文件加载成功。。。。。")
+time.sleep(1)
+print("预设推广次数为：",config['count'],"\n预设推广时间间隔为：",config['interval'],"\n预设推广地址为：",config['url'])
+time.sleep(1)
+luck=random.randint(1,100)
+print("你的幸运值为：",luck)
+ua=main.Openua('ua.txt').ua
+# 加载ua
+
+url = main.indexurl(config['url']).url
+# 加载文章
+
+timei=time.perf_counter()
+z=True
+cs = 0
+while(z):
+    uatemp=ua[(cs+luck)%(len(ua))]
+    indextemp=(url[(cs+(2*luck))%len(url)])
+    main.gogogo(indextemp,uatemp)
+    time.sleep(int(config['interval']))# 冷却时间
+    cs=cs+1
+    timen=(int(config['count'])-cs)*int(config['interval'])
+    print("\r推广次数",cs,"预计剩余时间：",timen,end="")
+    if cs>int(config['count']):
+        cs=0
+        z=False
+        print()
+        timei=int(time.perf_counter())-int(timei)
+        print("完成,共耗时：",timei)
